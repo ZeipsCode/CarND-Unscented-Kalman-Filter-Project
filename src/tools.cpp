a61b920079fe6e5f4f6963acf58134297442363a
zpr <Dennis.Zeipert@streetscooter.eu>
2018-07-01 16:07:14 +0200

    changed some parameters

---
+++ src/tools.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    
+    
+    if(estimations.size() == 0){
+        cout << "Invalid estimation size" << endl;
+        return rmse;
+    }
+    if (estimations.size() != ground_truth.size()) {
+        cout << "Ground truth vector wrong size" << endl;
+        return rmse;
+    }
+    
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+        
+        VectorXd residual = estimations[i] - ground_truth[i];
+        
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+    
+    //calculate the mean
+    rmse = rmse/estimations.size();
+    
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+    
+    //return the result
+    return rmse;
+}
